b09e1226f7a0019596320c72ab03cfb716624eee
moundiriharchafane <mondiriharchafane@gmail.com>
2023-08-20 09:51:38 -0700

    0x11. C - printf

--- printf.c
+++ printf.c
@@ -1,49 +1,66 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: number of bytes printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ak;
-	char *k, *start;
-	params_t params = PARAMS_INIT;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(ap, format);
+	if (format == NULL)
+		return (-1);
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (k = (char *)format; *k; k++)
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		init_params(&params, ak);
-		if (*k != '%')
+		if (format[i] != '%')
 		{
-			sum += _putchar(*k);
-			continue;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
-		start = k;
-		k++;
-		while (get_flag(k, &params)) /* while char at k is flag char */
-		{
-			p++; /* next char */
-		}
-		k = get_width(k, &params, ak);
-		k = get_precision(k, &params, ak);
-		if (get_modifier(k, &params))
-			k++;
-		if (!get_specifier(k))
-			sum += print_from_to(start, k,
-				params.l_modifier || params.h_modifier ? k - 1 : 0);
 		else
-			sum += get_print_func(k, ak, &params);
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
-	_putchar(BUF_FLUSH);
-	va_end(ak);
-	return (sum);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
