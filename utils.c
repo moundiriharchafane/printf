0f9953b1145aeebffbf0d790784dc68e9278c89a
abderrahmanot <abderrahmaot@gmail.com>
2023-08-23 13:46:56 -0700

    printf

--- utils.c
+++ utils.c
@@ -71,7 +71,7 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Casts a number to the specified size.
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
- * Return: Casted value of num
+ * Return: Casted value of num.
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
